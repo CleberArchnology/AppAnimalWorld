b68e4554ceeb3ab35c1f42fe0d81c0932f1b8faf
Your Name <railson.araujo.cipriano@gmail.com>
2021-06-16 22:34:39 -0300

    att

--- app/src/main/java/com/example/appanimaworld/AtualizaClienteActivity.java
+++ app/src/main/java/com/example/appanimaworld/AtualizaClienteActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AgendamentoActivity extends AppCompatActivity {
+public class AtualizaClienteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agendamento);
+        setContentView(R.layout.activity_atualiza_cliente);
     }
 }
