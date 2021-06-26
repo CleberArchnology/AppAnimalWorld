1ad8d7f0bface661b5999005ef6aceeb314a7460
unknown <cleberrodrigues014@gmail.com>
2021-06-26 13:16:28 -0300

    Att

--- app/src/main/java/com/example/appanimaworld/MainActivity.java
+++ app/src/main/java/com/example/appanimaworld/MainActivity.java
@@ -7,14 +7,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        referencia.child("pontos").setValue("100");
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Animal World");
         setSupportActionBar( toolbar );
